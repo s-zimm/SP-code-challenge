2b21b3fd6146f6d994ba53da32d684e8a62a9a0e
Seth <s.zim517@gmail.com>
2018-04-15 17:26:42 -0400

    Adds tiles

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,10 @@ const $navSelect = $('[data-nav-select]');
 const $menuDropdown = $('[data-nav-menu]');
 
 $navSelect.on('click', (event) => {
-    $(event.target).children().toggleClass('hidden dropdown-animation');
+    $navSelect.not(event.target)
+        .children()
+        .addClass('hidden')
+        .removeClass('dropdown-animation');
+    $(event.target).children()
+        .toggleClass('hidden dropdown-animation');
 });
