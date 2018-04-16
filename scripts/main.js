2589714b84a0e6fdfb07e9931cb195bd8055b0c6
Seth <s.zim517@gmail.com>
2018-04-16 09:55:38 -0400

    Modifies font and adds underline on hover to tiles

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const $navSelect = $('[data-nav-select]');
 const $menuDropdown = $('[data-nav-menu]');
+const $tileContainer = $('div.tile-container, div.long-tile-container');
 
 $navSelect.on('click', (event) => {
     $navSelect.not(event.target)
@@ -8,4 +9,11 @@ $navSelect.on('click', (event) => {
         .removeClass('dropdown-animation');
     $(event.target).children()
         .toggleClass('hidden dropdown-animation');
+});
+
+$tileContainer.hover(function() {
+    let theLink = $(this).find('a');
+    theLink.removeClass('naked');
+}, function() {
+    $(this).find('a').addClass('naked');
 });
